1a223fb9edfb796c0e217da04e03b405481c8842
primecloudez <primecloudez@gmail.com>
2026-06-13 13:59:48 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Managing FlowNation!",
+        name: "Managing Ayqwaa's Servers!",
         // Activity type number (0 = Playing).
         type: 1, 
       },
